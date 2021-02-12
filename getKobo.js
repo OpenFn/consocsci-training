f7c5ac2a464a93a52031e961e818d0a3a918821b
Taylor Downs <taylor@openfn.org>
2021-02-12 09:49:31 +0000

    Create getKobo.js

---
+++ getKobo.js
@@ -0,0 +1,55 @@
+//== Job to be used for fetching data from Kobo from ConSocSci Training acct on repeated, timer basis  ==//
+// This can be run on-demand at any time by clicking "run" //
+alterState(state => {
+  // Set a manual cursor if you'd like to only fetch data after this date.
+  state.data = {
+    surveys: [
+      ///====***FOR ADMINS TO UPDATE & CHANGE FORMS ***=====================//
+      //** Specify new forms to fetch here from the `wcs_training` account **//
+      //{ id: 'aMpW7wwRBRbtCfidtK2rRn', tag: 'bns_survey', name: 'Form Project Name', owner: 'wcs'},
+      {
+        id: 'adiNTJXFtpKEDGGZFMUtgQ', //unique Id from form URL
+        type: 'sharks_rays', //survey type/ template/ form group
+        name: 'Sharks and Rays Demo', //form name to display in the database
+        owner: 'openfn', //Kobo form Owner
+      }
+      //=================================================================//
+    ].map(survey => ({
+      formId: survey.id,
+      type: survey.type,
+      name: survey.name,
+      owner: survey.owner,
+      url: `https://kf.kobotoolbox.org/api/v2/assets/${survey.id}/data/?format=json`,
+      query: ``,
+    })),
+  };
+  return state;
+});
+
+each(dataPath('surveys[*]'), state => {
+  const { url, query, type, formId, name, owner } = state.data;
+  return get(`${url}${query}`, {}, state => {
+    state.data.submissions = state.data.results.map((submission, i) => {
+      return {
+        i,
+        // Here we append the tags defined above to the Kobo form submission data
+        formId: formId,
+        formType: type,
+        formName: name,
+        formOwner: owner,
+        body: submission,
+      };
+    });
+    const count = state.data.submissions.length;
+    console.log(`Fetched ${count} submissions from ${formId} (${type}).`);
+    //Once we fetch the data, we want to post each individual Kobo survey
+    //back to the OpenFn inbox to run through the jobs =========================
+    return each(dataPath('submissions[*]'), state => {
+      console.log(`Posting ${state.data.i + 1} of ${count}...`);
+      return post(state.configuration.openfnInboxUrl, {
+        body: state => state.data,
+      })(state);
+    })(state);
+    // =========================================================================
+  })(state);
+});
