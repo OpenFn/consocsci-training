db0b3a048226bc2c6eeba0a2fb7a51e9cee33f97
Aleksa Krolls <aleksagk@gmail.com>
2021-05-05 16:00:05 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobPracticeFarmers.js
+++ jobPracticeFarmers.js
@@ -15,7 +15,7 @@ upsert(
     KoboDataId: findValue({
       uuid: 'KoboDataId',
       relation: 'KoboDataForms',
-      where: { 'TableId':  state.data.tableId) }
+      where: { 'TableId':  dataValue('tableId') }
     }),
     SurveyDate: dataValue('body.today'), 
     Age: dataValue('body.age'), 
